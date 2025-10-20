3f143fa300e0ffbeef65bd6826516683e88c51b9
manubg <bgmanu2426@gmail.com>
2025-10-20 23:34:22 +0530

    fix: update allowedHosts in Vite config to include .azurewebsites.net

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: false,
-    allowedHosts: ['localhost', '.clackypaas.com', '.1024.sh'],
+    allowedHosts: ['localhost', '.azurewebsites.net'],
     hmr: {
       overlay: true,
     },
